af8e558d9d523976864b54f7d0bb443a080f1cd7
melmel6 <melina.siskou@gmail.com>
2023-01-13 13:45:41 +0100

    Adding a test for training data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,24 @@
 import pytest
 import torch
+from tests import _PATH_DATA
+import os
 
+def test_train_data():
+    train_data_path = os.path.join(_PATH_DATA, "./train.pt")  # root of data
+    dataset = torch.load(train_data_path)
 
-@pytest.mark.skip
-def test_something_about_data():
-    pass
+    # assert that all labels are represented
+    assert len(dataset['user']) == 1
+    assert len(dataset['movie']) == 1
+    assert len(dataset['user', 'rates', 'movie']) == 3
+    assert len(dataset['movie', 'rev_rates', 'user']) == 1
+
+    # assert size of each datapoint
+    assert dataset['user'].x.shape == torch.Size([671, 671])
+    assert dataset['movie'].x.shape == torch.Size([9025, 404])
+    assert dataset['user', 'rates', 'movie'].edge_index.shape == torch.Size([2, 79848])
+    assert dataset['user', 'rates', 'movie'].edge_label.shape == torch.Size([79848])
+    assert dataset['user', 'rates', 'movie'].edge_label_index.shape == torch.Size([2, 79848])
+    assert dataset['movie', 'rev_rates', 'user'].edge_index.shape == torch.Size([2, 79848])
+
+    
