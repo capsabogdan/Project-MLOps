e963c2bfd3e5e20542277637f20997d94756665f
ThomaSpyrou <spyroutommy@gmail.com>
2023-01-13 15:18:20 +0100

    Modified tests for data

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,16 +3,25 @@ import torch
 from tests import _PATH_DATA
 import os
 
-def test_train_data():
-    train_data_path = os.path.join(_PATH_DATA, "./train.pt")  # root of data
-    dataset = torch.load(train_data_path)
 
+train_data_path = os.path.join(_PATH_DATA, "./train.pt")  # root of data
+dataset = torch.load(train_data_path)
+
+
+def test_if_data_empty():
+    # test if load data is empty
+    assert len(dataset) != 0
+
+
+def test_train_data_labels():
     # assert that all labels are represented
     assert len(dataset['user']) == 1
     assert len(dataset['movie']) == 1
     assert len(dataset['user', 'rates', 'movie']) == 3
     assert len(dataset['movie', 'rev_rates', 'user']) == 1
 
+
+def test_train_datapoints():
     # assert size of each datapoint
     assert dataset['user'].x.shape == torch.Size([671, 671])
     assert dataset['movie'].x.shape == torch.Size([9025, 404])
@@ -21,4 +30,3 @@ def test_train_data():
     assert dataset['user', 'rates', 'movie'].edge_label_index.shape == torch.Size([2, 79848])
     assert dataset['movie', 'rev_rates', 'user'].edge_index.shape == torch.Size([2, 79848])
 
-    
