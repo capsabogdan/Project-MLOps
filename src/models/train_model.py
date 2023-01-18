13be2b8f3d52f20b7aa9990d0a6134c0bf486604
ThomaSpyrou <spyroutommy@gmail.com>
2023-01-18 17:41:25 +0100

    Training in docker, reading from bucket GCP

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,47 +1,56 @@
-
-import logging
 import os
-import sys
 import hydra
-from src.models import _PATH_DATA
-# import importlib.util
-
-
-import pandas as pd
-from arango import ArangoClient
-from tqdm import tqdm
-import numpy as np
-import itertools
-import requests
-import sys
-from arango import ArangoClient
-
 import torch
+import time
 import torch.nn.functional as F
-# from torch.nn import Linear
-# from arango import ArangoClient
-# import torch_geometric.transforms as T
-# from torch_geometric.nn import SAGEConv, to_hetero
-# from torch_geometric.transforms import RandomLinkSplit, ToUndirected
-# from sentence_transformers import SentenceTransformer
-# from torch_geometric.data import HeteroData
-import yaml
-
-
-
-# from google.cloud import storage  # type: ignore
+from google.cloud import storage 
 from omegaconf import DictConfig, OmegaConf
-
-import matplotlib.pyplot as plt
 from src.models.model import Model
 
 
 def weighted_mse_loss(pred, target, weight=None):
     weight = 1. if weight is None else weight[target].to(pred.dtype)
+
     return (weight * (pred - target.to(pred.dtype)).pow(2)).mean()
 
 
-# def test(data: torch_geometric.data.Data, model: torch.nn.Module) -> float:
+def push_data_to_cloud(bucket_name, file):
+    print("Downloading data...")
+    storage_client = storage.Client(project="zeroshots")
+    bucket = storage_client.bucket(bucket_name)
+
+    blob = bucket.blob(file)
+    blob.download_to_filename(file)
+
+
+def fetch_data_from_cloud(bucket_name, file_names):
+    path_to_download = "src/models/temp_data/"
+    print("Downloading data...")
+    storage_client = storage.Client(project="zeroshots")
+    bucket = storage_client.bucket(bucket_name)
+
+    if not os.path.exists(path_to_download):
+        os.mkdir(path_to_download)
+
+    for file in file_names:
+        blob = bucket.blob(file)
+        blob.download_to_filename(path_to_download + file)
+
+    print("Download complete! Sleeping for 5")
+    
+    time.sleep(5)
+
+
+def read_data():
+    # Load data
+    print("thomas", os.getcwd())
+    train_data = torch.load("src/models/temp_data/train.pt")
+    val_data = torch.load("src/models/temp_data/val.pt")
+    test_data = torch.load("src/models/temp_data/test.pt")
+
+    return train_data, test_data, val_data
+
+
 def test(data, model):
     """
     Evaluates model on data and returns accuracy.
@@ -49,8 +58,6 @@ def test(data, model):
     :param data: Data to evaluate on
     :return: accuracy
     """
-
-
     model.eval()
     pred = model(data.x_dict, data.edge_index_dict,
                  data['user', 'movie'].edge_label_index)
@@ -72,22 +79,14 @@ def train(config: DictConfig) -> None:
     # print = log.info
     # # wandb.init(project="Project-MLOps", entity="Project-MLOps")
 
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cpu"  #"cuda" if torch.cuda.is_available() else "cpu"
+    train_data, test_data, val_data = read_data()
 
     print("Training")
     print(f"configuration: \n {OmegaConf.to_yaml(config)}")
     hparams = config.experiments.hyperparams
     # wandb.config = hparams
     torch.manual_seed(hparams["seed"])
-    orig_cwd = hydra.utils.get_original_cwd()
-
-    # Load data
-
-
-    train_data = torch.load(os.path.join(_PATH_DATA, "train.pt"))
-    val_data = torch.load(os.path.join(_PATH_DATA, "val.pt"))
-    test_data = torch.load(os.path.join(_PATH_DATA, "test.pt"))
-
 
     # Model
     metadata = (['user', 'movie'],
@@ -103,10 +102,6 @@ def train(config: DictConfig) -> None:
         model.encoder(train_data.x_dict, train_data.edge_index_dict)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=hparams["lr"], weight_decay=5e-4)
-    
-
-
-
 
     # Train model
     for epoch in range(hparams["epochs"]):
@@ -121,7 +116,6 @@ def train(config: DictConfig) -> None:
         loss = weighted_mse_loss(pred, target, weight)
         loss.backward()
         optimizer.step()
-
         
         # Compute rmse
         train_rmse = test(train_data, model)
@@ -130,39 +124,10 @@ def train(config: DictConfig) -> None:
 
         print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Train: {train_rmse:.4f}, '
           f'Val: {val_rmse:.4f}, Test: {test_rmse:.4f}')
- #       wandb.log({"Training loss": loss})
+#       wandb.log({"Training loss": loss})
+#       wandb.log({"Training loss": loss})
 
-
-
-
-        # # Clear gradients
-        # optimizer.zero_grad()
-        # # Perform a single forward pass
-        # out = model(train_data.x_dict, train_data.edge_index_dict,
-        #          train_data['user', 'movie'].edge_label_index)
-        # target = train_data['user', 'movie'].edge_label
-        # # Compute the loss solely based on the training nodes
-
-
-        # loss = weighted_mse_loss(out, target, weight)
-        # # Derive gradients
-        # loss.backward()
-        # # Update parameters based on gradients
-        # optimizer.step()
-        # # Append results
-        # train_loss.append(loss.item())
-
-        # # Compute rmse
-        # train_rmse = test(train_data)
-        # val_rmse = test(val_data)
-        # test_rmse = test(test_data)
-
-#         # print
-#         print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Train: {train_rmse:.4f}, '
-#           f'Val: {val_rmse:.4f}, Test: {test_rmse:.4f}')
-#  #       wandb.log({"Training loss": loss})
-
-
+    orig_cwd = hydra.utils.get_original_cwd()
 
     # Save model
     directory = orig_cwd + "/models/"
@@ -171,9 +136,13 @@ def train(config: DictConfig) -> None:
     filename = directory + hparams["checkpoint_name"]
     checkpoint = {'hidden_channels': hparams["hidden_channels"],
                   'state_dict': model.state_dict()}
-    torch.save(checkpoint, filename)    
+    torch.save(checkpoint, filename)  
+
+
+
 
 
 
 if __name__ == "__main__":
-    train()    
+    fetch_data_from_cloud("movies-mlops-clean", ["train.pt", "test.pt", "val.pt"])
+    train()
